ab7e933f5634fbc41e5eeed1f8e6f3d70426edb6
Julius.M <favor.julius@gmail.com>
2021-11-05 13:27:44 -0400

    commit tree 50639733a0ca29131de208d6ba7dd45f92e34efe

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require "pry"
 def game_hash
   {
     home: {
@@ -127,52 +128,103 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored players_name
+  playerList = game_hash[:home][:players] + game_hash[:away][:players]  
+  playerList.find do |player|
+    if player[:player_name] == players_name
+     return player[:points]    
+    end 
+  end   
+end
+
+def shoe_size players_name
+  playerList = game_hash[:home][:players] + game_hash[:away][:players]  
+  playerList.find do |player|
+    if player[:player_name] == players_name
+     return player[:shoe]
+    end 
+  end   
+end
+
+def team_colors team
+  team = game_hash.find do |location, team_data|
+    team_data[:team_name] == team
+  end
+  team[1][:colors]
+end
+
+def team_names
+ game_hash.map do |location,team_data|
+  team_data[:team_name]
+ end
+end
+
+def player_numbers team
+  team_info  = game_hash.find do |location,team_data|
+   team_data[:team_name] == team
+  end 
+   team_info[1][:players].collect do |player|
+     player[:number]
+   end 
+end
+
+def player_stats name
+  all_players = game_hash[:home][:players] + game_hash[:away][:players]
+   all_players.find do |names|
+     names[:player_name] == name    
+   end
+end
+
+
 
 def all_players
   game_hash[:home][:players] +  game_hash[:away][:players]
 end
-def player_stats(player_name)
- all_players.find do |player| 
-    player[:player_name] == player_name 
-  end
-end
 
-def num_points_scored(player_name)
-  player = player_stats(player_name)
-  player[:points]
-end
 
-def shoe_size(player_name)
-  player = player_stats(player_name)
-  player[:shoe]
-end
 
-def find_team(team_name)
-  # #find on with a hash returns an array with the first key and value that match the condition
-  team_info = game_hash.find do |location, team_data|
-    team_data[:team_name] == team_name
-  end
-  # return just the value (team_data) from the .find method
-  team_info[1]
-end
+# def player_stats(player_name)
+#  all_players.find do |player| 
+#     player[:player_name] == player_name 
+#   end
+# end
 
-def team_colors(team_name)
-  team = find_team(team_name)
-  team[:colors]
-end
+# def num_points_scored(player_name)
+#   player = player_stats(player_name)
+#   player[:points]
+# end
 
-def team_names
-  game_hash.map do |location, team_data|
-    team_data[:team_name]
-  end
-end
+# def shoe_size(player_name)
+#   player = player_stats(player_name)
+#   player[:shoe]
+# end
 
-def player_numbers(team_name)
-  team = find_team(team_name)
-  team[:players].map do |player|
-    player[:number]
-  end
-end
+# def find_team(team_name)
+#   # #find on with a hash returns an array with the first key and value that match the condition
+#   team_info = game_hash.find do |location, team_data|
+#     team_data[:team_name] == team_name
+#   end
+#   # return just the value (team_data) from the .find method
+#   team_info[1]
+# end
+
+# def team_colors(team_name)
+#   team = find_team(team_name)
+#   team[:colors]
+# end
+
+# def team_names
+#   game_hash.map do |location, team_data|
+#     team_data[:team_name]
+#   end
+# end
+
+# def player_numbers(team_name)
+#   team = find_team(team_name)
+#   team[:players].map do |player|
+#     player[:number]
+#   end
+# end
 
 def big_shoe_player
   # https://ruby-doc.org/core-3.0.1/Enumerable.html#method-i-max_by
